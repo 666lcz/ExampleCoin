9f505c6d509910d5ed169e8279962b7beb6d5968
zhangolove <cl46@rice.edu>
2020-12-16 19:22:28 -0800

    add test

---
+++ token/test/TokenTest.js
@@ -0,0 +1,25 @@
+const Token = artifacts.require("Token")
+
+contract('Token', function(accounts) {
+    describe('Initial parameters are set correctly', function () {
+        it('name is set correctly', async function () {
+          return Token.deployed().then(async function (instance) {
+              expect(await instance.name()).to.equal('Joyce');
+          });
+        });
+
+        it('symbol is set correctly', async function () {
+            return Token.deployed().then(async function (instance) {
+                expect(await instance.symbol()).to.equal('JYZ');
+            });
+          });
+
+        it('total supply is set correctly', async function () {
+        return Token.deployed().then(async function (instance) {
+            const supply = (await instance.totalSupply()).toNumber();
+            expect(supply).to.equal(1000);
+        });
+        });
+    });
+
+})
