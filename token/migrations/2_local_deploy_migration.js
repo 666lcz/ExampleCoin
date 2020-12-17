556c50bb964c101144f9b3fd383f8ade6f8e266f
666lcz <cl46@rice.edu>
2020-12-16 21:57:33 -0800

    skeleton for token sale

--- token/migrations/2_local_deploy_migration.js
+++ token/migrations/2_local_deploy_migration.js
@@ -1,5 +1,7 @@
 const Token = artifacts.require("Token");
+const TokenSale = artifacts.require("TokenSale");
 
 module.exports = function (deployer) {
   deployer.deploy(Token);
+  deployer.deploy(TokenSale);
 };
